5cf1faf54d534dd560d36567aa574d63fb2428ca
23cqiao <23cqiao@athenian.org>
2020-05-07 16:41:18 +0000

    focus day

--- focus_day_project.py
+++ focus_day_project.py
@@ -1,2 +1,26 @@
 # Welcome to your Focus Day Project. Replace this comment with something that introduces the user to your project. Be sure to mention the Focus Day and your initials and graduation year. (ie This game is for Pool Volume Day and is written by ML '23.)
 # Also, be sure to use comments throughout your program. Use good programming practices, including organization, documentation and citation. Yes, you need to cite your sources! (You can do so using comments at the bottom of your code.)
+# Harry Potter Day, sorting hat
+def sortinghat(houses):
+    G=0
+    S=0
+    H=0
+    R=0
+print("Welcome to the sorting hat")
+print("I will ask you some questions and sort you into the right house.")
+print("Let's begin!")
+print("A Blue, B Red, C Green, D Yellow")
+def answers(A,B,C,D)
+Q1=print(str("What color above suits your personality the most? "))
+if Q1==A:
+    R+1
+elif Q1==B:
+    G+1
+elif Q1==C:
+    S+1
+elif Q1==D:
+    H+1
+
+
+    
+
