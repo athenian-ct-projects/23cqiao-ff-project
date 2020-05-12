153a70658617dcc484c2b5fe867788325f907db8
23cqiao <23cqiao@athenian.org>
2020-05-12 17:41:57 +0000

    focus day project

--- focus_day_project.py
+++ focus_day_project.py
@@ -10,7 +10,11 @@ print("Welcome to the sorting hat")
 print("I will ask you some questions and sort you into the right house.")
 print("Let's begin!")
 print("A Blue, B Red, C Green, D Yellow")
-def answers(A,B,C,D)
+def answers(A,B,C,D): #what should A,B,C,D be defined as here?
+    A=1
+    B=1
+    C=1
+    D=1
 Q1=print(str("What color above suits your personality the most? "))
 if Q1==A:
     R+1
@@ -20,6 +24,41 @@ elif Q1==C:
     S+1
 elif Q1==D:
     H+1
+print("A snake, B lion, C badger, D eagle ")
+Q2=print(str("Which of the animals below is your favorite? "))
+if Q2==A:
+    S+1
+elif Q2==B:
+    G+1
+elif Q2==C:
+    H+1
+elif Q3==D:
+    R+1
+print("A fire, B air, C water, D earth")
+Q3=print(str("Which element relates with you the most? "))
+if Q3==A:
+    G+1
+elif Q3==B:
+    R+1
+elif Q3==C:
+    S+1
+elif Q3==D:
+    S+1
+print("A analytical, B ambitous, C giving, D honest")
+Q4=print(str("Which of the following words describes you the most? "))
+if Q4==A:
+   R+1
+elif Q4==B:
+    S+1
+elif Q4==C:
+    H+1
+elif Q4==D:
+    G+1
+print("A talented, B clever, C brave, D determind")
+Q4=pint(str("Which of the qualities do you think is the key to success? "))
+ 
+
+
 
 
     
